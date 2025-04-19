add7133a5b6b0f0f0204bfe5da2a070a8b06781b
sabiabraham <156504128+sabiabraham@users.noreply.github.com>
2025-04-19 14:12:07 -0700

    Session 4 Prompt

---
+++ session4.py
@@ -0,0 +1,16 @@
+class MyFavoriteClass
+
+def__init__(self, arm_length, leg_length, num_eyes, has_tail, is_furry)
+self.arm_length = arm_length
+self.leg_length = leg_length
+self.num_eyes = num_eyes
+self.has_tail = has_tail
+self.is_furry = is_furry
+
+def describe(self):
+    print("This animal has:")
+    print(f"- Arm length: {self.arm_length} inches")
+    print(f"- Leg length: {self.leg_length} inches")
+    print(f"- Number of eyes: {self.eye_count}")
+    print(f"- Has a tail: {self.has_tail}")
+    print(f"- Is furry: {self.is_furry}")
