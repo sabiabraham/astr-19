839df89c47ced6eb8af46242c96f8ebaec827270
sabiabraham <156504128+sabiabraham@users.noreply.github.com>
2025-04-19 00:15:18 -0700

    Session 2 Prompt

---
+++ session2.py
@@ -0,0 +1,12 @@
+#sum of two floating point numbers
+sum_result = 2.5 + 3.3
+print("Sum:", sum_result)
+print(type(sum_result))
+
+diff_result = 10 - 5
+print("Difference:", diff_result)
+print(type(diff_result))
+
+prod_result = 3 * 3
+print("Product:", prod_result)
+print(type(prod_result))
